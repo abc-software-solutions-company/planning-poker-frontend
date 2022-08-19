a1f02ed5309d039d07eba2b6a35580985f494093
Luan Nguyen <a000010@abcsoftwarecompany.com>
2022-08-19 10:30:05 +0700

    feat: 🎸 [PLAN-17] Create module chart

--- src/components/voting/index.tsx
+++ src/components/voting/index.tsx
@@ -1,8 +1,9 @@
 import React, {useRef} from 'react';
 
-import VoteCard from '@/components/cards';
+// import VoteCard from '@/components/cards';
 import LogoCopy from '@/components/icons/copy';
 import Button from '@/core-ui/button';
+import Chart from '@/core-ui/chart';
 import Heading from '@/core-ui/heading';
 import InputText from '@/core-ui/input-text';
 import ModalCreate from '@/core-ui/modal';
@@ -35,7 +36,7 @@ const VoteRoom: React.FC<IProps> = ({dataUsers}) => {
           <div className="content">
             <div className="left-content">
               <Heading as="h4">Story name</Heading>
-              <div className="card-holder">
+              {/* <div className="card-holder">
                 <VoteCard>0</VoteCard>
                 <VoteCard>1</VoteCard>
                 <VoteCard>2</VoteCard>
@@ -44,7 +45,58 @@ const VoteRoom: React.FC<IProps> = ({dataUsers}) => {
                 <VoteCard>8</VoteCard>
                 <VoteCard>13</VoteCard>
                 <VoteCard>21</VoteCard>
-              </div>
+              </div>  */}
+              <Chart
+                className="chart-holder"
+                data={{
+                  type: 'doughnut',
+                  options: {
+                    rotation: 0.5 * Math.PI - (95 / 180) * Math.PI,
+                    responsive: false,
+                    plugins: {
+                      datalabels: {
+                        display: true,
+                        backgroundColor: '#ccc',
+                        borderRadius: 3,
+                        font: {
+                          color: 'red',
+                          weight: 'bold'
+                        }
+                      },
+                      doughnutlabel: {
+                        labels: [
+                          {
+                            text: '550',
+                            font: {
+                              size: 20,
+                              weight: 'bold'
+                            }
+                          },
+                          {
+                            text: 'total'
+                          }
+                        ]
+                      },
+                      legend: {
+                        position: 'bottom',
+                        labels: {
+                          usePointStyle: true,
+                          boxWidth: 8
+                        }
+                      }
+                    }
+                  },
+                  data: {
+                    labels: ['Red', 'Blue', 'Yellow'],
+                    datasets: [
+                      {
+                        data: [330, 30, 30],
+                        backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)']
+                      }
+                    ]
+                  }
+                }}
+              />
             </div>
             <div className="right-content">
               <h5 className="title">Waiting for Admin vote</h5>
