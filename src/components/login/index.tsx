e65596e1f9715b3e9efc6f562c7cc8b7ad0ed654
Luan Nguyen <a000010@abcsoftwarecompany.com>
2022-08-22 14:04:54 +0700

    feat: 🎸 [FrontEnd] Create API Handle

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
+import {useForm} from 'react-hook-form';
 
 import Heading from '@/components/heading';
-import InputText from '@/components/input-text';
 import Button from '@/core-ui/button';
+import {createUser} from '@/data/client/user.client';
+import {ICreateUser} from '@/types';
 
 import styles from './style.module.scss';
 
 const Login: React.FC = () => {
+  const {register, handleSubmit} = useForm();
+
   return (
     <>
-      <div className={`${styles.login}`}>
+      <form className={`${styles.login}`} onSubmit={handleSubmit(data => createUser(data as ICreateUser))}>
         <div className="container">
           <Heading as="h1">PLANNING POKER</Heading>
           <div className="content">
             <Heading as="h4">Let&apos;s start !</Heading>
-            <InputText className="form-input" placeholder="Enter your name"></InputText>
+            <input className="form-input" placeholder="Enter your name" {...register('name')} />
             <Button className="btn">Enter</Button>
           </div>
           <div className="footer">Copyright © 2022 By ABC Software Solutions Company.</div>
         </div>
-      </div>
+      </form>
     </>
   );
 };
