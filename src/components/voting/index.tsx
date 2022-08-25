6f831fae5e91373592dc3c7a39e234cf100882ea
Luan Nguyen <a000010@abcsoftwarecompany.com>
2022-08-25 14:33:43 +0700

    feat: 🎸 [FrontEnd] Fix Join Link input UI

--- src/components/voting/index.tsx
+++ src/components/voting/index.tsx
@@ -1,10 +1,10 @@
 import React, {useRef} from 'react';
 
-// import VoteCard from '@/components/cards';
+import VoteCard from '@/components/cards';
 import LogoCopy from '@/components/icons/copy';
 import ModalStory from '@/components/modal-stories';
 import Button from '@/core-ui/button';
-import Chart from '@/core-ui/chart';
+// import Chart from '@/core-ui/chart';
 import Heading from '@/core-ui/heading';
 import Input from '@/core-ui/input';
 import {IVoteUser} from '@/types';
@@ -34,7 +34,7 @@ const VoteRoom: React.FC<IProps> = ({dataUsers}) => {
           <div className="content">
             <div className="left-content">
               <Heading as="h4">Story name</Heading>
-              {/* <div className="card-holder">
+              <div className="card-holder">
                 <VoteCard>0</VoteCard>
                 <VoteCard>1</VoteCard>
                 <VoteCard>2</VoteCard>
@@ -43,8 +43,8 @@ const VoteRoom: React.FC<IProps> = ({dataUsers}) => {
                 <VoteCard>8</VoteCard>
                 <VoteCard>13</VoteCard>
                 <VoteCard>21</VoteCard>
-              </div>  */}
-              <Chart
+              </div>
+              {/* <Chart
                 className="chart-holder"
                 data={{
                   type: 'doughnut',
@@ -82,7 +82,7 @@ const VoteRoom: React.FC<IProps> = ({dataUsers}) => {
                     }
                   }
                 }}
-              />
+              /> */}
             </div>
             <div className="right-content">
               <h5 className="title">Result</h5>
