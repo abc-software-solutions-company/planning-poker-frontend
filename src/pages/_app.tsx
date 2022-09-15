d0b286e3039ef94a0c02fdad567e881e31a8374f
Xuân Huy <101651216+nxhuy1806@users.noreply.github.com>
2022-09-15 11:50:38 +0700

    [FrontEnd] Fix progress router (#87)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,33 +2,51 @@ import '@/vendors/bootstrap/bootstrap.scss';
 import '@/vendors/tailwindcss/style.scss';
 import '@/vendors/menu/style.scss';
 import '@/vendors/abc-icons/dist/abc.scss';
+import 'nprogress/nprogress.css';
 
 import type {AppProps} from 'next/app';
 import {useRouter} from 'next/router';
 import {appWithTranslation} from 'next-i18next';
-import NextNProgress from 'nextjs-progressbar';
-import React from 'react';
+import nProgress from 'nprogress';
+import {useEffect} from 'react';
 
 import DefaultSeo from '@/components/common/seo/default-seo';
 import {AuthProvider} from '@/contexts/auth';
-// import QueryProvider from '@/contexts/query.provider';
 
 const Noop: React.FC = ({children}: React.PropsWithChildren<any>) => <>{children}</>;
 
 const CustomApp = ({Component, pageProps: {session, ...pageProps}}: AppProps) => {
   const router = useRouter();
   const Layout = (Component as any).Layout || Noop;
+  nProgress.configure({
+    minimum: 0.3,
+    easing: 'ease',
+    speed: 800,
+    showSpinner: true
+  });
+  useEffect(() => {
+    const progress = () => {
+      nProgress.start();
+    };
+    const doneProgress = () => {
+      nProgress.done();
+    };
+    router.events.on('routeChangeStart', progress);
+    router.events.on('routeChangeComplete', doneProgress);
+    router.events.on('routeChangeError', doneProgress);
+    return () => {
+      router.events.off('routeChangeStart', progress);
+      router.events.off('routeChangeComplete', doneProgress);
+      router.events.off('routeChangeError', doneProgress);
+    };
+  }, []);
   return (
-    // <QueryProvider pageProps={pageProps}>
     <AuthProvider>
       <DefaultSeo />
-      <NextNProgress color="#3D99D3" />
-      {/* <GoogleTagManager /> */}
       <Layout pageProps={pageProps}>
         <Component {...pageProps} key={router.route} />
       </Layout>
     </AuthProvider>
-    // </QueryProvider>
   );
 };
 
