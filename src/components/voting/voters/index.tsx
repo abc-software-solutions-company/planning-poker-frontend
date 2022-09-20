ce7dab2a693d70637346973c86561d0bfbe498d3
phuocvuive09 <a000014@abcsoftwarecompany.com>
2022-09-20 09:13:07 +0700

    feat: 🎸 [FE] Online and offline for user

--- src/components/voting/voters/index.tsx
+++ src/components/voting/voters/index.tsx
@@ -19,7 +19,10 @@ const VoteUser: React.FC<IProps> = ({name, className, votePoint, host, isComplet
     <>
       <div className={cn('player-status', className)}>
         <div className="player-info">
-          <Icon className={host ? 'text-red-400' : ''} name="ico-avatar" size={24} />
+          <div className="icon online">
+            <Icon className={host ? 'text-red-400' : ''} name="ico-avatar" size={24} />
+          </div>
+
           <div className="name">
             {name} {host ? <span className="host">Host</span> : ''}
           </div>
