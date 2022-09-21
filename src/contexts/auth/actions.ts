1a239dfe340460f5433c16ce059fafe980bfe420
Phuoc Huu <110363738+phuocvuive09@users.noreply.github.com>
2022-09-21 10:02:46 +0700

    feat: 🎸 Optimize auth state (#129)

--- src/contexts/auth/actions.ts
+++ src/contexts/auth/actions.ts
@@ -1,10 +1,15 @@
-import {IState} from './state';
 import * as types from './types';
+
+interface IPayload {
+  id: string;
+  name: string;
+}
 
 export interface IAction {
   type: typeof types.LOGIN;
-  payload: IState;
+  payload: IPayload;
 }
-export const login = (payload: IState): IAction => {
+
+export const login = (payload: IPayload): IAction => {
   return {type: types.LOGIN, payload};
 };
