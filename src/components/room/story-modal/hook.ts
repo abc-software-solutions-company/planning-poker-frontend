84cfb9b158c636e6def2bbbafdfbea41a6bddbed
Tin Tran <tindl88@gmail.com>
2022-10-10 11:56:33 +0700

    Update SEO data

--- src/components/room/story-modal/hook.ts
+++ src/components/room/story-modal/hook.ts
@@ -26,6 +26,7 @@ export default function useStoryModal({setOpenModal}: IProps) {
   const {
     register,
     setValue,
+    getValues,
     handleSubmit,
     reset,
     formState: {errors, dirtyFields}
@@ -62,6 +63,7 @@ export default function useStoryModal({setOpenModal}: IProps) {
   };
 
   useEffect(() => {
+    const {type} = getValues();
     if (!roomData?.story?.type) setStoryType(type);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [dirtyFields.type]);
---
+++ src/configs/site.config.ts
@@ -0,0 +1,62 @@
+import {merge} from 'lodash-es';
+
+const WEBSITE_NAME = 'Planning Pocker';
+const WEBSITE_URL = process.env.NEXT_PUBLIC_SITE_URL;
+const COMPANY_NAME = 'ABC Software Solutions';
+const COMPANY_URL = 'https://abcsoftwarecompany.com';
+const COMPANY_SOCIAL = {
+  facebook: 'https://www.facebook.com/abcsoftwaresolutionscompany',
+  twitter: 'https://www.linkedin.com/company/abc-software-solutions-company'
+};
+
+const companyApps = [
+  ...Object.values(COMPANY_SOCIAL),
+  'https://voteuserstory.com',
+  'https://sharetodolist.com',
+  'https://pickonename.com'
+];
+
+const organizationDefault = {
+  '@context': 'https://schema.org',
+  '@type': 'Organization',
+  founder: 'Khanh Mai',
+  url: COMPANY_URL,
+  logo: `${COMPANY_URL}/android-chrome-512x512.png`,
+  image: `${COMPANY_URL}/og-abc.png`,
+  description: 'Simple solutions for complex problems. Improve work performance every day for your company',
+  address: {
+    '@type': 'PostalAddress',
+    addressCountry: 'Vietnam',
+    addressRegion: 'Khanh Hoa',
+    addressLocality: 'Nha Trang',
+    postalCode: '650000'
+  }
+};
+
+const websiteDefault = {
+  '@context': 'https://schema.org',
+  '@type': 'WebSite',
+  datePublished: 'September 2022',
+  url: WEBSITE_URL,
+  sameAs: companyApps.filter(x => x !== WEBSITE_URL)
+};
+
+export const siteSettings = {
+  name: WEBSITE_NAME,
+  logo: `${WEBSITE_URL}/android-chrome-512x512.png`,
+  cover: {url: `${WEBSITE_URL}/og-img.png`, width: 1200, height: 630, alt: WEBSITE_NAME},
+  url: WEBSITE_URL,
+  defaultLanguage: 'en',
+  author: {
+    name: `${COMPANY_NAME} Company`,
+    websiteUrl: COMPANY_URL,
+    email: 'hello@abcsoftwarecompany.com',
+    socials: COMPANY_SOCIAL
+  },
+  description: 'Planning Poker is an application for many participants to vote on user stories.',
+  keyword: 'planning pocker, vote user story, realtime, scrum pocker, agile estimation, story point estimations',
+  schemaJsonLd: {
+    organization: merge(organizationDefault, {name: COMPANY_NAME}),
+    website: merge(websiteDefault, {name: WEBSITE_NAME})
+  }
+};
